ad8de2ad9a657961af7ec9948a67fe7e9b4260af
Marc Sladek <marc.sladek@synventis.com>
2017-09-20 18:26:10 +0200

    Computop raw object

---
+++ src/main/java/com/celements/payment/raw/Computop.java
@@ -0,0 +1,114 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.celements.payment.raw;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Strings;
+
+public class Computop implements PaymentRawObject {
+
+  private int id = 0;
+
+  private String origHeader = "";
+
+  private String origMessage = "";
+
+  private String txnId = "";
+
+  private String merchantId = "";
+
+  private int length = 0;
+
+  private String data = "";
+
+  @Enumerated(EnumType.STRING)
+  private EProcessStatus processStatus = EProcessStatus.Unknown;
+
+  public Computop() {
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getOrigHeader() {
+    return origHeader;
+  }
+
+  public void setOrigHeader(String origHeader) {
+    this.origHeader = Strings.nullToEmpty(origHeader);
+  }
+
+  public String getOrigMessage() {
+    return origMessage;
+  }
+
+  public void setOrigMessage(String origMessage) {
+    this.origMessage = Strings.nullToEmpty(origMessage);
+  }
+
+  public String getTxnId() {
+    return txnId;
+  }
+
+  public void setTxnId(String txnId) {
+    this.txnId = Strings.nullToEmpty(txnId);
+  }
+
+  public String getMerchantId() {
+    return merchantId;
+  }
+
+  public void setMerchantId(String merchantId) {
+    this.merchantId = Strings.nullToEmpty(merchantId);
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setLength(int length) {
+    this.length = length;
+  }
+
+  public String getData() {
+    return data;
+  }
+
+  public void setData(String data) {
+    this.data = Strings.nullToEmpty(data);
+  }
+
+  public EProcessStatus getProcessStatus() {
+    return processStatus;
+  }
+
+  public void setProcessStatus(EProcessStatus processStatus) {
+    this.processStatus = MoreObjects.firstNonNull(processStatus, EProcessStatus.Unknown);
+  }
+
+}
